7a12b5d464accbc73f14386c2fdec2e99c9c3fa6
gitwork <249476157@qq.com>
2022-05-07 21:31:32 +0800

    完成聊天机器人

---
+++ index.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="index">
+<img src="./../assets/img/header.png" class="header" alt="">
+<div class="btn-group">
+    <button class="btn">分享</button>
+    <button class="btn btn-primary">充值</button>
+     <button class="btn">活动详情</button>
+</div>
+</div>
+</template>
+<script>
+ export default {
+     name: 'index',
+     data(){
+         return {
+
+         }
+     }
+ }
+</script>
+<style>
+.index{
+    background-color:#FFC93A;
+    height:100vh;
+}
+.btn-group{
+    padding-top: .34rem;
+    text-align: center;
+}
+ .nickname{
+    position:absolute;
+    top:6rem;
+    left: 50%;
+    transform: 
+    translateX(-50%);
+    font-size:18px;
+  }
+</style>
